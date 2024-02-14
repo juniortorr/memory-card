bfc4b80a033eb02d7de364fb5e2bc7878ccd3918
juniortorr <juniortorr77@gmail.com>
2024-02-13 23:10:05 -0800

    - set up .env

--- memory-card-app/.eslintrc.cjs
+++ memory-card-app/.eslintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  node: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
@@ -13,9 +14,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
   },
-}
+};
--- memory-card-app/src/App.jsx
+++ memory-card-app/src/App.jsx
@@ -1,5 +1,23 @@
 import './assets/styles/App.css';
+const apiKey = process.env.REACT_APP_API_KEY;
+import { md5 } from 'js-md5';
+const hash = md5(process.env.REACT_APP_API_KEY);
 
-function App() {}
+function App() {
+  async function logData() {
+    const response = await fetch(
+      `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=40e806804049a89d6bedae9e247da779&hash=${hash}`,
+      {
+        mode: 'cors',
+      }
+    );
+    const json = await response.json();
+    console.log(json);
+  }
+  logData();
+}
 
 export default App;
+
+// md5('Message to hash');
+//
